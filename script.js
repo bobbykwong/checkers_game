8655c074e7631ce4563c5dd727f74ded1c96d770
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-03-29 17:55:25 +0800

    added win condition

--- script.js
+++ script.js
@@ -24,31 +24,136 @@ const movePiece = (event) => {
         // Store new position in variable
         storeNewPosition(event.target.className);
 
+        // If no obstruction.
         moveNoObstruction();
 
         // If obstruction,
         moveWithCapture();
 
-         // Choose new position to move to.
-
-
-
-         // Else If no obstruction,
-
-         // display new Board
+        // display new Board
         displayPieces();
 
+        // Check win condition
+        checkWin();
 
         // Reset selectPiecePosition and New Position for next move.
         selectPiecePosition = [];
         newPosition = [];
 
     }
-
-
-
-
 }
+
+
+/////////////////////////Create Board
+
+const main = document.querySelector('main');
+const buildBoard = document.createElement('div');
+buildBoard.className = 'board';
+main.appendChild(buildBoard);
+
+for (var i = 0; i < 8; i++) {
+
+    const buildRow = document.createElement('div');
+
+    buildRow.className = `length row${i}`;
+
+    buildBoard.appendChild(buildRow);
+
+    // let colCounterOdd = 1;
+    // let colCounterEven = 0;
+    for (var a = 0; a < 8; a++) {
+
+        const buildBox = document.createElement('div');
+
+        // console.log("colCounter is " + colCounter);
+
+        if ( (i%2===0) && (a%2 !== 0) ) {
+            buildBox.className = `dark row${i} col${a}`;
+            // colCounterOdd++;
+        }
+        else if( (i%2===0) && (a%2===0) ){
+            //light grids are non-plyable grids.
+            // Classname only serves for styling purposes, therefore numbering not required.
+            buildBox.className = 'light';
+        }
+        else if( (i%2 !== 0) && (a%2===0) ){
+            buildBox.className = `dark row${i} col${a}`
+            // colCounterEven++;
+        }
+        else if( (i%2 !== 0) && (a%2 !== 0) ){
+            buildBox.className = 'light';
+        }
+
+        buildBox.textContent = ""
+        buildBox.addEventListener('click', movePiece)
+        buildRow.appendChild(buildBox);
+    }
+    // colCounterOdd = 1;
+    // colCounterEven = 0;
+};
+
+
+//////////////////////////////////////Create Virtual Board to keep track of pieces
+
+// 0 means empty grid, 1 is Player 1 and 2 is Player 2;
+let board = [];
+for(var b=0; b<8; b++){
+    board[b] = [];
+    for (var c = 0; c < 8; c++) {
+        if((b%2===0) && (c%2 !== 0) && b<3){
+            board[b].push(1);
+        }
+        else if ( (b%2 !== 0) && (c%2===0) && b<3){
+            board[b].push(1);
+        }
+        else if ((b%2===0) && (c%2 !== 0) && b > 4){
+            board[b].push(2);
+        }
+        else if ((b%2 !== 0) && (c%2===0) && b > 4){
+            board[b].push(2);
+        }
+        else{
+            board[b].push(0);
+        }
+
+    }
+};
+
+
+///////////////////////////////////////Create function to position pieces on UI
+
+const displayPieces = () =>{
+    for (var d=0; d<8; d++){
+        for (var e=0; e<8; e++){
+            // Specify Grid
+            const grid = document.getElementsByClassName(`row${d} col${e}`)[0];
+            // Loop may run over light grids which are undefined based on above classname selection
+            if (grid != undefined) {
+                // Manipulate Grid to show piece or remain as empty grid
+                if(board[d][e] === 1){
+                    grid.classList.add('player1');
+                }
+                else if(board[d][e] === 2){
+                    grid.classList.add('player2');
+                }
+                // Remove piece from initial position after movement
+                else if(board[d][e] === 0 && ( grid.classList.contains('player1') || grid.classList.contains('player2')) ){
+                    if(grid.classList.contains('player1')){
+                        grid.classList.remove('player1');
+                    }
+                    else if(grid.classList.contains('player2')){
+                        grid.classList.remove('player2');
+                    }
+                }
+            }
+        }
+    }
+}
+
+displayPieces()
+
+
+
 
 // Store position of selected piece into variable
 const selectPiece = (piece) => {
@@ -249,7 +354,7 @@ const moveWithCapture = () => {
         }
 
         // If row is even
-        else if(selectPiecePosition[1] %2 != 0){
+        else if(selectPiecePosition[1] %2 === 0){
             // If row is even and first box, only one legal move
             if( (board[selectPiecePosition[1]+1][selectPiecePosition[2]+1] === 2) && (selectPiecePosition[2] === 1) ){
                 if(newPosition[1] === 3){
@@ -297,7 +402,7 @@ const moveWithCapture = () => {
         }
 
         // If row is odd
-        else if(selectPiecePosition[1] %2 === 0){
+        else if(selectPiecePosition[1] %2 != 0){
             // If row is odd and first box, only one legal move
             if( (board[selectPiecePosition[1]+1][selectPiecePosition[2]+1] === 2) && (selectPiecePosition[2] === 0) ){
                 if(newPosition[1] === 2){
@@ -407,7 +512,7 @@ const moveWithCapture = () => {
         }
 
         // If row is odd
-        else if(selectPiecePosition[1] %2 === 0){
+        else if(selectPiecePosition[1] %2 != 0){
             // If row is odd and first box, only one legal move
             if( (board[selectPiecePosition[1]-1][selectPiecePosition[2]+1] === 1) && (selectPiecePosition[2] === 0) ){
                 if(newPosition[1] === 2){
@@ -455,118 +560,17 @@ const moveWithCapture = () => {
         }
     }
 
-
 }
 
 
+// Function to check if any player has won
 
-
-
-/////////////////////////Create Board
-
-const main = document.querySelector('main');
-const buildBoard = document.createElement('div');
-buildBoard.className = 'board';
-main.appendChild(buildBoard);
-
-for (var i = 0; i < 8; i++) {
-
-    const buildRow = document.createElement('div');
-
-    buildRow.className = `length row${i}`;
-
-    buildBoard.appendChild(buildRow);
-
-    // let colCounterOdd = 1;
-    // let colCounterEven = 0;
-    for (var a = 0; a < 8; a++) {
-
-        const buildBox = document.createElement('div');
-
-        // console.log("colCounter is " + colCounter);
-
-        if ( (i%2===0) && (a%2 !== 0) ) {
-            buildBox.className = `dark row${i} col${a}`;
-            // colCounterOdd++;
-        }
-        else if( (i%2===0) && (a%2===0) ){
-            //light grids are non-plyable grids.
-            // Classname only serves for styling purposes, therefore numbering not required.
-            buildBox.className = 'light';
-        }
-        else if( (i%2 !== 0) && (a%2===0) ){
-            buildBox.className = `dark row${i} col${a}`
-            // colCounterEven++;
-        }
-        else if( (i%2 !== 0) && (a%2 !== 0) ){
-            buildBox.className = 'light';
-        }
-
-        buildBox.textContent = ""
-        buildBox.addEventListener('click', movePiece)
-        buildRow.appendChild(buildBox);
+const checkWin = () => {
+    const allPieces = board.flat();
+    if(!(allPieces.includes(1))){
+        console.log('Player 1 wins');
     }
-    // colCounterOdd = 1;
-    // colCounterEven = 0;
-};
-
-
-//////////////////////////////////////Create Virtual Board to keep track of pieces
-
-// 0 means empty grid, 1 is Player 1 and 2 is Player 2;
-let board = [];
-for(var b=0; b<8; b++){
-    board[b] = [];
-    for (var c = 0; c < 8; c++) {
-        if((b%2===0) && (c%2 !== 0) && b<3){
-            board[b].push(1);
-        }
-        else if ( (b%2 !== 0) && (c%2===0) && b<3){
-            board[b].push(1);
-        }
-        else if ((b%2===0) && (c%2 !== 0) && b > 4){
-            board[b].push(2);
-        }
-        else if ((b%2 !== 0) && (c%2===0) && b > 4){
-            board[b].push(2);
-        }
-        else{
-            board[b].push(0);
-        }
-
-    }
-};
-
-
-///////////////////////////////////////Create function to position pieces on UI
-
-const displayPieces = () =>{
-    // debugger;
-    for (var d=0; d<8; d++){
-        for (var e=0; e<8; e++){
-            // Specify Grid
-            const grid = document.getElementsByClassName(`row${d} col${e}`)[0];
-            // Loop may run over light grids which are undefined based on above classname selection
-            if (grid != undefined) {
-                // Manipulate Grid to show piece or remain as empty grid
-                if(board[d][e] === 1){
-                    grid.classList.add('player1');
-                }
-                else if(board[d][e] === 2){
-                    grid.classList.add('player2');
-                }
-                // Remove piece from initial position after movement
-                else if(board[d][e] === 0 && ( grid.classList.contains('player1') || grid.classList.contains('player2')) ){
-                    if(grid.classList.contains('player1')){
-                        grid.classList.remove('player1');
-                    }
-                    else if(grid.classList.contains('player2')){
-                        grid.classList.remove('player2');
-                    }
-                }
-            }
-        }
+    else if(!(allPieces.includes(2))){
+        console.log('Player 2 wins');
     }
 }
-
-displayPieces()
